2765bd15025e0f1b7e6a3afb1372395a222bf811
Himarry <m.chihiro0314@gmail.com>
2025-06-12 20:35:34 +0900

    バージョン番号を0.1.0から0.1.2に更新し、Pythonのバージョン要件を明示的に指定。バージョンアップ用のスクリプトを追加。

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ytwrap",
-    version="0.1.0",
+    version="0.1.2",
     description="YouTube Data API v3 ラッパーライブラリ",
     author="Himarry",
     packages=find_packages('ytwrap'),
@@ -10,5 +10,14 @@ setup(
     install_requires=[
         "google-api-python-client"
     ],
-    python_requires=">=3.7",
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
 )
